9c3662cf82b95d7236189893ed1543897d6f2331
HOMMA Takashi <hommatks@gmail.com>
2019-01-25 17:05:17 +0900

    hogrhoge

--- src/statement.js
+++ src/statement.js
@@ -8,7 +8,7 @@ function statement(invoice, plays) {
         minimumFractionDigits: 2,
     }).format;
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
@@ -25,7 +25,7 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
         switch (playFor(aPerformance).type) {
             case 'tragedy':
