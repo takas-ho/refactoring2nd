1937db9728b51ef264e3b8bb1c5f14184cd18bf3
HOMMA Takashi <hommatks@gmail.com>
2019-01-26 10:44:55 +0900

    Move Function(2/2)

--- src/statement.js
+++ src/statement.js
@@ -17,7 +17,7 @@ function statement(invoice, plays) {
 function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${ perf.audience } seats)\n`;
+        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${ perf.audience } seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -51,17 +51,13 @@ function renderPlainText(data, plays) {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
         return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
     }
 
     function amountFor(aPerformance) {
         let result = 0;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case 'tragedy':
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -76,7 +72,7 @@ function renderPlainText(data, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
         return result;
     }
