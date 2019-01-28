0e1774a9b49b098fba9932cc08b939bd30372d31
HOMMA Takashi <hommatks@gmail.com>
2019-01-28 21:04:05 +0900

    now it's easy to write an HTML version

--- src/statement.test.js
+++ src/statement.test.js
@@ -1,4 +1,6 @@
-const statement = require('./statement');
+const statementMain = require('./statement');
+const statement = statementMain.statement;
+const htmlStatement = statementMain.htmlStatement;
 
 test('本に乗っていたサンプル例を実行（機能性のテストはしていない）', () => {
   const plays = {
@@ -93,5 +95,46 @@ Amount owed is $1,260.00
 You earned 14 credits
 `;
   const result = statement(invoice, plays);
+  expect(result).toEqual(expected);
+});
+
+
+test('本に乗っていたサンプル例を実行(html)（機能性のテストはしていない）', () => {
+  const plays = {
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    'as-like': { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' },
+  };
+
+  const invoice = {
+    customer: 'BigCo',
+    performances: [
+      {
+        playID: 'hamlet',
+        audience: 55,
+      },
+      {
+        playID: 'as-like',
+        audience: 35,
+      },
+      {
+        playID: 'othello',
+        audience: 40,
+      },
+    ],
+  };
+
+  const expected =
+`<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr>
+  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
+`;
+  const result = htmlStatement(invoice, plays);
   expect(result).toEqual(expected);
 });
